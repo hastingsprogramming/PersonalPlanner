991c667d8a93c6111a3e1e61ce6ec816bcdec836
Bradley Beasley <bradleybeasley2000@gmail.com>
2020-04-22 17:41:06 +0100

    Attempted Implementing MVVM
    With these changes, I have included Caliburn.Micro and implemented a simple MVVM window

---
+++ PersonalPlanner/ViewModels/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalPlanner.ViewModels
+{
+    class LoginViewModel : Screen
+    {
+        public void CloseWindow() => this.TryClose();
+    }
+}
--- PersonalPlanner/Views/LoginView.xaml.cs
+++ PersonalPlanner/Views/LoginView.xaml.cs
@@ -13,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace PersonalPlanner
+namespace PersonalPlanner.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for LoginView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LoginView : Window
     {
-        public MainWindow()
+        public LoginView()
         {
             InitializeComponent();
         }
