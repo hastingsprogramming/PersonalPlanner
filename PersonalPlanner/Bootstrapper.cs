561687be7674c4ae2b51eb1504d92537a1279948
Bradley Beasley <bradleybeasley2000@gmail.com>
2020-04-25 13:33:34 +0100

    Removed Some Dependencies

--- PersonalPlanner/Bootstrapper.cs
+++ PersonalPlanner/Bootstrapper.cs
@@ -3,10 +3,7 @@ using PersonalPlanner.Helpers;
 using PersonalPlanner.ViewModels;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 
