371c5148e649f362a4af11b0fdb52832960ff7a4
Bradley Beasley <bradleybeasley2000@gmail.com>
2020-05-07 14:45:40 +0100

    Episode 11, Commit 1
    Navigation and some UI implementation

--- PersonalPlanner/ViewModels/RootViewModel.cs
+++ PersonalPlanner/ViewModels/RootViewModel.cs
@@ -19,6 +19,18 @@ namespace PersonalPlanner.ViewModels
 
         public void Loaded()
         {
+            SignOut();
+        }
+
+        public void DashboardShow() => ActivateItem(IoC.Get<DashboardViewModel>());
+        public void FinanceShow() => ActivateItem(IoC.Get<FinanceViewModel>());
+        public void NotesShow() => ActivateItem(IoC.Get<NotesViewModel>());
+        public void EventsShow() => ActivateItem(IoC.Get<EventsViewModel>());
+        public void ProjectsShow() => ActivateItem(IoC.Get<ProjectsViewModel>());
+
+        public void SignOut()
+        {
+            Reset();
             var vm = IoC.Get<LoginViewModel>();
             var r = WindowManager.ShowDialog(vm, null, null);
 
@@ -26,6 +38,12 @@ namespace PersonalPlanner.ViewModels
             {
                 TryClose();
             }
+
+        }
+
+        void Reset()
+        {
+            ActivateItem(IoC.Get<DashboardViewModel>());
         }
     }
 }
